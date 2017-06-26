9720da1c52a1896508adcb24caacfc7b0765acec
王声远 <wangshengyuan@isoffice.com>
2017-06-26 14:05:17 +0800

    Initial Commit

---
+++ TimerManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimerManager
+//
+//  Created by 王声远 on 2017/6/26.
+//  Copyright © 2017年 王声远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimerManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimerManager
+//
+//  Created by 王声远 on 2017/6/26.
+//  Copyright © 2017年 王声远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
